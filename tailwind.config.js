e3c6b1a69755606f6f8cbb82f67c4cc9263625af
FarhadNawab <farhadnawab.official@gmail.com>
2022-03-11 15:56:35 +0500

    basic structure

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            first: '#ECECE6',
+            second: '#E9F0F8',
+            third: '#121212',
+            fourth: '#BD4523',
+            fifth: '#F5CB46',
+        },
+        fontFamily: {
+            clash: 'clashdisplay',
+        }
+      }
+    }
+  }
